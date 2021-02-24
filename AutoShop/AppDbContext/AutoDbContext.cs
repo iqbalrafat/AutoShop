4401084c3de983b6c90400d011653f8d604e08bb
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-02-23 23:52:25 -0700

    [AutoShop]--Added DbContext

---
+++ AutoShop/AppDbContext/AutoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoShop.AppDbContext
+{
+    public class AutoDbContext:DbContext
+    {
+        public AutoDbContext( DbC
+    }
+}
