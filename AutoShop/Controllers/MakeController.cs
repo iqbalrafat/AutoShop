87e2f264f610260a861c55a175d8644a5713fd4f
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-02-28 13:04:38 -0700

    [AutoShop]-- created Make Controller

---
+++ AutoShop/Controllers/MakeController.cs
@@ -0,0 +1,20 @@
+﻿using AutoShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoShop.Controllers
+{
+    public class MakeController : Controller
+    {
+        //make/bikes    (this is the route how the view will be called "/controller/action)
+        public IActionResult Bikes()
+           
+        {
+            Make make = new Make { Id = 1, Name = "Harley Davidson" };
+            return View(make);
+        }
+    }
+}
