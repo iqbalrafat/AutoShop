19df75b0703f5ac0233eb91f015dd2c4a07d59a2
iqbalrafat <iqbalrafat@yahoo.com>
2021-04-04 23:45:28 -0600

    [AutoShop]--Added Edit Method

--- AutoShop/Controllers/ModelController.cs
+++ AutoShop/Controllers/ModelController.cs
@@ -46,5 +46,14 @@ namespace AutoShop.Controllers
             _db.SaveChanges();
             return RedirectToAction(nameof(Index));
         }
+        public IActionResult Edit(int id)
+        {
+            ModelVM.Model = _db.Models.Include(m => m.Make).SingleOrDefault(m => m.Id == id);
+            if(ModelVM.Model==null)
+            {
+                return NotFound();
+            }
+            return View (ModelVM);
+        }
     }
 }
